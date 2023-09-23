6ec02ce4e945bee1d40d9f4fa429620d3c49173a
StephanyKleen <stephanykleen@gmail.com>
2023-09-23 14:20:27 -0700

    Learning to add react router for components to have specific paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import TiktokListItems from "./TiktokListItems.js";
 import Button from "./Button.js";
+import Home from "./components/Home.js";
+import Users from "./components/Users.js";
+import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <>
@@ -7,9 +10,17 @@ function App() {
       <h2>A Summary of TikTok Videos I've Watch</h2>
       <TiktokListItems />
       <h3>Which one is your favorite?</h3>
+
       <Button name="#1" />
       <Button name="#2" />
       <Button name="#3" />
+
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/users">
+        <Users />
+      </Route>
     </>
   );
 }
